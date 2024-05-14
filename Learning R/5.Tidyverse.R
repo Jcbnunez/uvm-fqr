29b467bc795df1a7910d8539b56002e6ad8340bc
JCB Nunez <jcnunez@JCBs-MacBook-Air.local>
2024-05-14 10:29:08 -0400

    builtindats2

---
+++ Learning R/5.Tidyverse.R
@@ -0,0 +1,47 @@
+#Tidyverse
+
+library(tidyverse)
+
+iris
+iris %>% head()
+iris$Species
+
+iris %>% filter(Species == "setosa")
+iris %>% filter(Petal.Width > 1.6)
+
+iris$Petal.Width %>% min()
+iris %>% filter(Petal.Width > 1.6) %>% .$Petal.Width %>% min()
+
+## Grouping
+
+iris %>%
+  group_by(Species) %>%
+  summarise(avPetal = mean(Petal.Width))
+
+
+iris %>%
+  group_by(Species) %>%
+  summarise(avPetal = mean(Petal.Width),
+            sdPetal = sd(Petal.Width),
+            minPetal = min(Petal.Width)
+            )
+
+# mutate
+iris %>%
+  mutate(mynewvar = Petal.Width+10)
+
+
+iris %>%
+  mutate(classifier = 
+           case_when(Petal.Width < 1.1 ~ "small", 
+                     TRUE ~ "big")
+           )
+
+
+
+
+
+
+  
+
+
